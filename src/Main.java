62da914785251c44828200f3c61ca3e60b896fe4
YUrkij <urok21887@gmail.com>
2024-09-01 17:09:42 +0500

    Home work 7-8 lesson

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        byte MyByte; //объявление
-        MyByte = - 128; //инициализация
+import java.util.Random;
+import java.util.Scanner;
 
-        short MyShort = 127;
-        int MyInteger = 127_902;
-        long MyLong = 3_000_000_000L;
+        public class Main {
+            public static void main(String[] args) {
+                /*
+                byte MyByte; //объявление
+                MyByte = - 128; //инициализация
 
-        float MyFloat = 3.0f;
-        double MyDouble = 3.0;
+                short MyShort = 127;
+                int MyInteger = 127_902;
+                long MyLong = 3_000_000_000L;
 
-        char MyChar = 'a';
-        char TestChar = '\n';
+                float MyFloat = 3.0f;
+                double MyDouble = 3.0;
 
-        String MyStrung = "Hi world!!!";
-        boolean MyBool = true;
+                char MyChar = 'a';
+                char TestChar = '\n';
 
-        
-        System.out.println("Числовые : " + MyByte + " " + MyShort + " " + MyInteger + " " + MyLong);
-        System.out.println("Дробные : " + MyFloat + " " + MyDouble);
-        System.out.println("Знаковые + строка : " +MyChar + " " + TestChar + " " + MyStrung);
-        System.out.println("Логические : " + MyBool);
-
-        MyInteger = 22;
-        MyShort = 7;
-        int ResultInt = MyInteger + MyShort;
-        System.out.println(MyInteger + " + " + MyShort + " = " + ResultInt);
-        ResultInt = MyInteger - MyShort;
-        System.out.println(MyInteger + " - " + MyShort + " = " + ResultInt);
-        ResultInt = MyInteger / MyShort;
-        System.out.println(MyInteger + " / " + MyShort + " = " + ResultInt + " Целочисленное деление");
-        ResultInt = MyInteger % MyShort;
-        System.out.println(MyInteger + " % " + MyShort + " = " + ResultInt + " Остаток от деления");
-        double ResultDou = (double) MyInteger / MyShort;
-        System.out.println(MyInteger + " / " + MyShort + " = " + ResultDou + " Деление без остатка" + "\n");
-
-        MyBool = true;
-        boolean MyBln = !MyBool;
-        System.out.println("!" + MyBool + " = " + MyBln );
+                String MyStrung = "Hi world!!!";
+                boolean MyBool = true;
 
 
-    }
-}
+                System.out.println("Числовые : " + MyByte + " " + MyShort + " " + MyInteger + " " + MyLong);
+                System.out.println("Дробные : " + MyFloat + " " + MyDouble);
+                System.out.println("Знаковые + строка : " +MyChar + " " + TestChar + " " + MyStrung);
+                System.out.println("Логические : " + MyBool);
+                */
+                // Пункт 1
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Введите первое число для арифметических операций: ");
+                int myInt1 = sc.nextInt();
+                System.out.print("Введите первое число для арифметических операций: ");
+                int myInt2 = sc.nextInt();
+                System.out.println(myInt1 + " + " + myInt2 + " = " + (myInt1 + myInt2));
+                System.out.println(myInt1 + " - " + myInt2 + " = " + (myInt1 - myInt2));
+                System.out.println(myInt1 + " / " + myInt2 + " = " + (myInt1 / myInt2) + " Целочисленное деление");
+                System.out.println(myInt1 + " % " + myInt2 + " = " + (myInt1 % myInt2) + " Остаток от деления");
+                System.out.println(myInt1 + " / " + myInt2 + " = " + ((double) myInt1 / myInt2) + " Деление без остатка" + "\n");
+                // Пункт 2
+                Random rnd = new Random();
+                myInt1 = rnd.nextInt(Character.MIN_CODE_POINT, Character.MAX_CODE_POINT);
+                myInt2 = rnd.nextInt(Character.MIN_CODE_POINT, Character.MAX_CODE_POINT);
+                int myInt3 = rnd.nextInt(Character.MIN_CODE_POINT, Character.MAX_CODE_POINT);
+                int myInt4 = rnd.nextInt(Character.MIN_CODE_POINT, Character.MAX_CODE_POINT);
+                char ch1 = (char) myInt1, ch2 = (char) myInt2, ch3 = (char) myInt3, ch4 = (char) myInt4;
+                System.out.print(ch1);
+                System.out.print(ch2);
+                System.out.print(ch3);
+                System.out.println(ch4);
+                // Пункт 3
+                long ln = Long.MAX_VALUE;
+                myInt1 = (int) ln;
+                System.out.println(ln + " : long");
+                System.out.println(myInt1 + " : переполненный int");
+
+            }
+        }
